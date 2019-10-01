d8a45c1a6624fa578ff422d31ec07a8736d3ba13
jorgeeder <jorgeeder.js@gmail.com>
2019-10-01 15:38:10 -0300

    Setting up Structure

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",  // responsavel por alterar as funcionalidades do javascript que o navegador não entende
+    "@babel/preset-react" // transformar as funcionalidades que o navegador não entende do react, exemplo: jsx 
+  ],
+}
