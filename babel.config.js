d5d4fd4978d8dc6358e0519a5278b53112a14ad2
jorgeeder <jorgeeder.js@gmail.com>
2019-10-02 12:16:43 -0300

    Class Components

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,7 @@ module.exports = {
     "@babel/preset-env",  // responsavel por alterar as funcionalidades do javascript que o navegador não entende
     "@babel/preset-react" // transformar as funcionalidades que o navegador não entende do react, exemplo: jsx 
   ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
+  ]
 }
